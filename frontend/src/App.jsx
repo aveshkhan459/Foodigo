357469c991052cc00bb70db903e4e41f93579f05
aveshkhan459 <aaveshkhan0678@gmail.com>
2026-07-17 16:15:13 +0530

    add backend url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,7 +25,7 @@ import { useEffect } from 'react'
 import { io } from 'socket.io-client'
 import { setSocket } from './redux/userSlice'
 
-export const serverUrl="https://foodigo-gwu2.onrender.com"
+export const serverUrl=import.meta.env.VITE_API_URL
 function App() {
     const {userData}=useSelector(state=>state.user)
     const dispatch=useDispatch()
